006290a976a83f72fc4b31f36a78a82a3d58d11b
s68122250060-cyber <s68122250060@ssru.ac.th>
2026-02-12 13:57:27 +0700

    first commit

---
+++ src/TestCircleRectangle1.java
@@ -0,0 +1,11 @@
+public class TestCircleRectangle1 {
+  public static void main(String[] args) {
+    Circle1 circle = new Circle1(1);
+    System.out.println("A circle " + circle.toString());
+    System.out.println("The color is " + circle.getColor());
+    System.out.println("The radius is " + circle.getRadius());
+    System.out.println("The area is " + circle.getArea());
+    System.out.println("The diameter is " + circle.getDiameter());
+
+  }
+}
